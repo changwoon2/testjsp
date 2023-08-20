3da9e9f4db1249e8f0aa6d353fff02ae9fe4db03
changwoon2 <changwoon4@naver.com>
2023-08-20 23:24:47 +0900

    Initial commit

---
+++ src/test/java/com/example/testjsp/TestjspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestjspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
